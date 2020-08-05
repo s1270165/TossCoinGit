505befec47ca2979572cd12a28f256750868a1ec
前田悠貴 <maedaharuki@MacBook-Pro.local>
2020-08-05 14:21:15 +0900

    User name added

--- tosscoin.c
+++ tosscoin.c
@@ -5,9 +5,13 @@
 int main(){
 
   int i, head=0, tail=0;
+  char name[15];
 
   srand((unsigned int)time(NULL));
 
+  printf("Who are you?\n");
+  scanf("%s", name);
+  printf("Hello,%s!\n", name);
   printf("Tossing a coin...\n");
   for(i=0; i<3; i++)
   {
