22cbec58e377d58e37783425366c9778dab3c688
前田悠貴 <maedaharuki@MacBook-Pro.local>
2020-08-05 14:25:12 +0900

    Victory message added

--- tosscoin.c
+++ tosscoin.c
@@ -5,9 +5,13 @@
 int main(){
 
   int i, head=0, tail=0;
+  char name[15];
 
   srand((unsigned int)time(NULL));
 
+  printf("Who are you?\n");
+  scanf("%s", name);
+  printf("Hello,%s!\n", name);
   printf("Tossing a coin...\n");
   for(i=0; i<3; i++)
   {
@@ -24,6 +28,8 @@ int main(){
   }
 
   printf("Heads:%d, Tails:%d\n", head, tail);
+  if( head > tail ) printf("you won!\n");
+  else printf("you lost!\n");
 
 return 0;
 }
